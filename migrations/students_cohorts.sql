97d87e3532bd3cdbf63e709ede86e962466a367e
Mitch Lum <mklum88@gmail.com>
2020-01-06 21:21:23 +0000

    recreated tables and added a query to find all students without github name

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,19 +1,18 @@
 CREATE TABLE cohort (
   id SERIAL PRIMARY KEY NOT NULL,
-  name varchar(255) NOT NULL,
-  start_date date,
-  end_date date
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
 );
-
 
 CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
-  name varchar(255) NOT NULL,
-  email varchar(255),
-  phone varchar(32),
-  github varchar(255),
-  start_date date,
-  end_date date,
-  cohort_id integer REFERENCES cohort(id) ON DELETE CASCADE
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohort(id) ON DELETE CASCADE
 );
 
